b517db09a6dbc135be4ffca8d33789d29cbd4e6a
RoginaIrshaidat <115452721+RoginaIrshaidat@users.noreply.github.com>
2023-01-31 16:05:09 -0800

    Controller Task

---
+++ 31-Jan/31-Jan/Controllers/DefaultController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _31_Jan.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        //public FileResult DownloadImage(string imageName)
+        //{
+        //    var imgPath = Path.Combine(Server.MapPath("~/Image"), imageName);
+        //    return File(imgPath, "Task/jpg", imageName);
+        //}
+
+        public string Img()
+        {
+            return "<h3>Click on the image to download it : </h3>" +
+                "<a href='downloadImage'><img src='../Image/Task.jpg' /></a>";
+        }
+        public void downloadImage()
+        {
+            var imagePath = Server.MapPath("../Image/Task.jpg");
+            Response.AddHeader("Content-Disposition", "attachment;filename=Task.jpg");
+            Response.WriteFile(imagePath);
+            Response.End();
+        }
+
+        public string AboutUs()
+        {
+            return "The first task in MVC ";
+        }
+        public string Contact()
+        {
+            return "Contact ";
+        }
+    }
+}
---
+++ 31-Jan/31-Jan/Controllers/RoginaController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _31_Jan.Controllers
+{
+    public class RoginaController : Controller
+    {
+        // GET: Rogina
+       
+        public string Name()
+        {
+            return "My name is : Rogina Irshaidat ";
+        }
+        public int ID()
+        {
+            return 32 ;
+        }
+        public string Majour()
+        {
+            return "My majour is : Computer Science ";
+        }
+        public double Salary()
+        {
+            return 450.50;
+        }
+    }
+}
