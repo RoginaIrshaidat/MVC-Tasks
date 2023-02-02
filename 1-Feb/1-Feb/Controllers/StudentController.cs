bc61b858a01f7834a2c9e76ac38c6f7597fc33b8
RoginaIrshaidat <115452721+RoginaIrshaidat@users.noreply.github.com>
2023-02-01 16:21:22 -0800

    View and Models

---
+++ 1-Feb/1-Feb/Controllers/StudentController.cs
@@ -0,0 +1,24 @@
+﻿using _1_Feb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _1_Feb.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            List<Models.Student> student = new List<Student>();
+            student.Add(new Student { ID=30,Name="Razan",Majour="Computer Enginer",Faculity="IT"});
+            student.Add(new Student { ID = 32, Name = "Rogina", Majour = "Computer Science", Faculity = "IT" });
+            student.Add(new Student { ID = 3, Name = "Amer", Majour = "Computer Science", Faculity = "IT" });
+
+
+            return View(student);
+        }
+    }
+}
---
+++ 1-Feb/1-Feb/Models/Student .cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _1_Feb.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Majour { get; set; }
+        public string Faculity { get; set; }
+    }
+}
