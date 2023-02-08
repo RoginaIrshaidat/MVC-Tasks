4677beb86cfadf66f496f40c96e498f238d7b286
RoginaIrshaidat <115452721+RoginaIrshaidat@users.noreply.github.com>
2023-02-07 16:56:58 -0800

    7-Feb Task

---
+++ layoutTask/layoutTask/Controllers/ChairsController.cs
@@ -0,0 +1,21 @@
+﻿using layoutTask.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace layoutTask.Controllers
+{
+
+    public class ChairsController : Controller
+    {
+        // GET: Chairs
+        MVCEntities1 mVC = new MVCEntities1();
+        public ActionResult Index()
+        {
+            var chair= mVC.Chairs.FirstOrDefault();
+            return View(chair);
+        }
+    }
+}
